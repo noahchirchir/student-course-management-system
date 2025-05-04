0dfb5ab8644b2275f4208c184600e29a3d20e316
noahchirchir <161455450+noahchirchir@users.noreply.github.com>
2025-05-04 21:06:27 +0300

    Update create_tables.sql

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,3 +1,36 @@
---create_database.sql
--- This script creates a new database named 'course_management'.
-CREATE DATABASE course_management;
+--create table for students
+CREATE TABLE Students (
+  student_id INT PRIMARY KEY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  email VARCHAR(100),
+  date_of_birth DATE
+);
+
+--create table for instructors
+CREATE TABLE Instructors (
+  instructor_id INT PRIMARY KEY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  email VARCHAR(100)
+);
+
+--create table for courses
+CREATE TABLE Courses (
+  course_id INT PRIMARY KEY,
+  course_name VARCHAR(255),
+  course_description TEXT,
+  instructor_id INT,
+  FOREIGN KEY (instructor_id) REFERENCES Instructors(instructor_id)
+);
+
+--create table for enrollments
+CREATE TABLE Enrollments (
+  enrollment_id INT PRIMARY KEY,
+  student_id INT,
+  course_id INT,
+  enrollment_date DATE,
+  grade CHAR(1),
+  FOREIGN KEY (student_id) REFERENCES Students(student_id),
+  FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);
